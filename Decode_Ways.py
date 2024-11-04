299c6360e3840d2ca3daa8b09be3f863062fd88e
xinrantaoangela <xinran.tao2001@gmail.com>
2024-11-04 12:14:13 -0500

    three medium level problems

---
+++ Decode_Ways.py
@@ -0,0 +1,13 @@
+def numDecodings(self, s: str) -> int:
+        if not s or s[0] == "0":
+            return 0
+        n = len(s)
+        dp = [0] * (n + 1)
+        dp[0] = 1
+        for i in range(1, n + 1):
+            if s[i - 1] != "0":
+                dp[i] += dp[i - 1]
+            if i > 1 and s[i - 2 : i] >= "10" and s[i - 2 : i] <= "26":
+                dp[i] += dp[i - 2]
+
+        return dp[n]
